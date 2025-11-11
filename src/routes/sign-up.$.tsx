55862e3dda7e6fa6dd5cdf5efd9e8774984aebb2
Taariq Elliott <codebytaariq@gmail.com>
2025-11-11 03:29:04 -0500

    First commit with fixes for Tanstack start and clerk button components

---
+++ src/routes/sign-up.$.tsx
@@ -0,0 +1,14 @@
+import { SignUp } from "@clerk/tanstack-react-start";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/sign-up/$")({
+	component: Page,
+});
+
+function Page() {
+	return (
+		<main className="flex items-center justify-center min-h-screen">
+			<SignUp />
+		</main>
+	);
+}
