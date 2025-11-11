5b0f0afdf693babce0030c72ba46927a0a98c6d3
Taariq Elliott <codebytaariq@gmail.com>
2025-11-11 11:41:55 -0500

    Made sample data table

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,19 +4,30 @@ import {
 	SignInButton,
 	UserButton,
 } from "@clerk/tanstack-react-start";
+import { convexQuery } from "@convex-dev/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { api } from "convex/_generated/api";
 
 export const Route = createFileRoute("/")({
 	component: Home,
 });
 
 function Home() {
+	const { data } = useSuspenseQuery(convexQuery(api.tasks.get, {}));
+
 	return (
 		<div>
 			<h1>Index Route</h1>
 			<SignedIn>
 				<p>You are signed in</p>
 				<UserButton />
+
+				<div>
+					{data.map(({ _id, text }) => (
+						<div key={_id}>{text}</div>
+					))}
+				</div>
 			</SignedIn>
 			<SignedOut>
 				<p>You are signed out</p>
